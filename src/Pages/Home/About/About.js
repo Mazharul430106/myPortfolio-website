191d9b65431f0d52b8270192a4eb0e5c4d76e65a
Mazharul Islam <mazharul430@gmail.com>
2022-12-10 01:36:28 -0800

    added about section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+<i></i>
 
 const About = () => {
     return (
         <div>
-            <h1>This is About Page</h1>
+            <div className="title">
+                <h1 style={{ marginTop: '50px', marginBottom: '50px', textAlign: 'center' }} className='text-5xl font-semibold '>About Me</h1>
+            </div>
+            <div className="hero ">
+                <div className="hero-content flex-col lg:flex-row w-full">
+                    <div className="w-full">
+                        <img src='https://i.ibb.co/fSkvHFD/IMG20221209125057-1-removebg-preview.png' alt="" />
+                    </div>
+
+                    <div className='w-full flex items-center'>
+                        <p>Hi I am Mazharul Islam, i am a Front-End-Developer As a Front-End Developer I can develop any front-end website application. Speciallysingle page application for react js with mongodb database . and I can clean code optimization, which is understandable for any developer, My extensive knowledge of HTML, Css,  Bootstrap, React Bootstrap, Tailwind Css,Javascript, React js, Mongodb, with familiar problem solving skills.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>This is Banner section</h1>
+        <div className="hero" style={{marginTop:'30px', marginBottom: '30px'}}>
+            <div className="hero-content flex-col lg:flex-row w-full">
+
+                <div className="w-full">
+                    <h1 className="text-5xl font-bold pb-5">Hi! I am,</h1>
+                    <h1 className="text-5xl font-bold"> Mazharul Islam</h1>
+                    <p className="py-6">Front-End-Developer</p>
+                    <input type="button" value="download resume" className='btn btn-primary text-white' />
+                </div>
+
+                <div className='w-full'>
+                  <img style={{width: '430px', height: '450px', borderRadius:'50%'}} className='flex items-center mx-auto' src="https://i.ibb.co/d7TJ4jK/90058212-107913660835813-3283847757328023552-n.jpg" alt="" />
+                </div>
+               
+            </div>
         </div>
     );
 };
